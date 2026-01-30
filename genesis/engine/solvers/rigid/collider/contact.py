5eba9b85cff7917abe201a88d49686e184787f8c
Hugh Perkins <hughperkins@gmail.com>
2026-01-30 10:51:12 -0500

    use atomic_add for n_contacts

--- genesis/engine/solvers/rigid/collider/contact.py
+++ genesis/engine/solvers/rigid/collider/contact.py
@@ -176,7 +176,9 @@ def func_add_contact(
     collider_info: array_class.ColliderInfo,
     errno: array_class.V_ANNOTATION,
 ):
-    i_c = collider_state.n_contacts[i_b]
+    # Atomically allocate contact slot to avoid race conditions when multiple threads
+    # process collisions from the same environment (e.g., with global collision queue)
+    i_c = ti.atomic_add(collider_state.n_contacts[i_b], 1)
     if i_c < collider_info.max_contact_pairs[None]:
         friction_a = geoms_info.friction[i_ga] * geoms_state.friction_ratio[i_ga, i_b]
         friction_b = geoms_info.friction[i_gb] * geoms_state.friction_ratio[i_gb, i_b]
@@ -193,8 +195,6 @@ def func_add_contact(
         )
         collider_state.contact_data.link_a[i_c, i_b] = geoms_info.link_idx[i_ga]
         collider_state.contact_data.link_b[i_c, i_b] = geoms_info.link_idx[i_gb]
-
-        collider_state.n_contacts[i_b] = i_c + 1
     else:
         errno[i_b] = errno[i_b] | 0b00000000000000000000000000000010
 
